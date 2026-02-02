f3e8c0958d5c092094a90c8ae48a8eea6d1dd19b
cr0iss4nt <809dan4ik@gmail.com>
2026-02-02 20:05:22 +0300

    Made verb inflection functions

--- inflector.py
+++ inflector.py
@@ -12,8 +12,12 @@ def inflect(word, morph: pymorphy3.MorphAnalyzer(), word_features: set):
     inflected_word = parsed_word.inflect(word_features)
     return inflected_word[0] if inflected_word is not None else 'Такой формы не существует!'
 
+
+
 def inflect_noun(noun, morph: pymorphy3.MorphAnalyzer(), case, number):
     return inflect(noun, morph, {case, number})
+
+
 
 def inflect_adjective_singular(adjective, morph: pymorphy3.MorphAnalyzer(), case, gender):
     return inflect(adjective, morph, {case, gender})
@@ -21,7 +25,20 @@ def inflect_adjective_singular(adjective, morph: pymorphy3.MorphAnalyzer(), case
 def inflect_adjective_plural(adjective, morph: pymorphy3.MorphAnalyzer(), case):
     return inflect(adjective, morph, {case})
 
-# TODO: make verb inflection
-def inflect_verb(verb, morph: pymorphy3.MorphAnalyzer(), mood, person, tense, gender, number):
-    return inflect(verb, morph, {mood, person, tense, gender, number})
+
+
+def inflect_verb_imperative(verb, morph: pymorphy3.MorphAnalyzer(), number):
+    return inflect(verb, morph, {number, 'excl'})
+
+def inflect_verb_past_singular(verb, morph: pymorphy3.MorphAnalyzer(), gender):
+    return inflect(verb, morph, {gender, 'sing', 'past'})
+
+def inflect_verb_past_plural(verb, morph: pymorphy3.MorphAnalyzer()):
+    return inflect(verb, morph, {'plur', 'past'})
+
+def inflect_verb_present(verb, morph: pymorphy3.MorphAnalyzer(), person, number):
+    return inflect(verb, morph, {'pres', person, number})
+
+def inflect_verb_future(verb, morph: pymorphy3.MorphAnalyzer(), person, number):
+    return inflect(verb, morph, {'futr', person, number})
 
