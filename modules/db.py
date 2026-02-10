ab986e4cb8061e8199568bf7091c169c35960186
cr0iss4nt <809dan4ik@gmail.com>
2026-02-10 18:25:58 +0300

    Optimized DB connections in text import

--- modules/db.py
+++ modules/db.py
@@ -5,6 +5,8 @@ import pymorphy3
 from modules.lexemizer import text_to_lexemes
 from modules.word_analyzer import get_base_and_ending
 
+import time
+
 DATABASE_NAME = 'dictionary.db'
 
 def init_db():
@@ -41,15 +43,23 @@ def get_words(query = ''):
     connection = sqlite3.connect(DATABASE_NAME)
     cursor = connection.cursor()
 
+    t1 = time.time()
+
     cursor.execute("SELECT * FROM Dictionary WHERE word LIKE ? ORDER BY word", ('%' + query + '%',))
     words = cursor.fetchall()
 
     connection.close()
 
+    dt = time.time()-t1
+    print(f"Fetched words (query='{query}') in {dt} seconds")
+
     return words
 
-def add_normalized_word(word, part_of_speech, base, ending):
-    connection = sqlite3.connect(DATABASE_NAME)
+def add_normalized_word(word, part_of_speech, base, ending, connection=None):
+    has_outer_connection = True
+    if connection is None:
+        has_outer_connection = False
+        connection = sqlite3.connect(DATABASE_NAME)
     cursor = connection.cursor()
 
     try:
@@ -62,17 +72,24 @@ def add_normalized_word(word, part_of_speech, base, ending):
     except:
         print(f"Error adding word: {word}")
 
-    connection.commit()
-    connection.close()
+    if not has_outer_connection:
+        connection.commit()
+        connection.close()
 
 def add_words_from_text(text, morph: pymorphy3.MorphAnalyzer()):
     lexemes = text_to_lexemes(text, morph)
+    connection = sqlite3.connect(DATABASE_NAME)
+    t1 = time.time()
     for lexeme in lexemes:
-        add_word(lexeme, morph)
+        add_word(lexeme, morph, connection=connection)
+    dt = time.time()-t1
+    connection.commit()
+    connection.close()
+    print(f"Added words from text in {dt} seconds")
 
-def add_word(word, morph: pymorphy3.MorphAnalyzer()):
+def add_word(word, morph: pymorphy3.MorphAnalyzer(), connection=None):
     part_of_speech = morph.parse(word)[0].tag.POS
-    add_normalized_word(word, part_of_speech, '', '')
+    add_normalized_word(word, part_of_speech, '', '', connection=connection)
 
 # change the base and the ending of the word
 def edit_word(word, base, ending):
--- modules/lexemizer.py
+++ modules/lexemizer.py
@@ -2,15 +2,18 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import pymorphy3
 
-RUSSIAN_LETTERS = 'ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДДЖЭЯЧСМИТЬБЮЁйцукенгшщзхъфывапролджэячсмитьбюё'
+import time
+
+RUSSIAN_LETTERS = set('ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДДЖЭЯЧСМИТЬБЮЁйцукенгшщзхъфывапролджэячсмитьбюё')
+STOP_WORDS = set(stopwords.words('russian'))
+
 
 def is_russian_text(text):
-    return not set(text) - set(RUSSIAN_LETTERS)
+    return not set(text) - RUSSIAN_LETTERS
 
 def tokenize_text(text):
     tokens = word_tokenize(text)
-    stop_words = set(stopwords.words('russian'))
-    filtered_tokens = [word for word in tokens if word not in stop_words and is_russian_text(word)]
+    filtered_tokens = [word for word in tokens if word not in STOP_WORDS and is_russian_text(word)]
     return filtered_tokens
 
 def tokens_to_lexemes(tokens, morph: pymorphy3.MorphAnalyzer()):
@@ -21,7 +24,10 @@ def tokens_to_lexemes(tokens, morph: pymorphy3.MorphAnalyzer()):
     return sorted(list(lexemes))
 
 def text_to_lexemes(text, morph: pymorphy3.MorphAnalyzer()):
+    t1 = time.time()
     tokens = tokenize_text(text)
     lexemes = tokens_to_lexemes(tokens, morph)
+    dt = time.time()-t1
+    print(f"Text lexemized in {dt} seconds")
     return lexemes
 
